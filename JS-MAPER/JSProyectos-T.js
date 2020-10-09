62c7c01d1b9ffc5bbeeba663ca450cbfaff62cdb
Lara258 <lara.luisla@gmail.com>
2020-10-09 12:48:36 -0500

    se agrego una pantalla de carga en index y se cambiaron nombres de archivos js, ademas se agrego la funcion de cambiarl el color de servicios cuando se haga hover en su submenu

---
+++ JS-MAPER/JSProyectos-T.js
@@ -0,0 +1,66 @@
+$('.carousel-proyectos').owlCarousel({
+    loop:true,
+    margin:10,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout: 6000,
+    touchDrag: false,
+    mouseDrag:false,
+    smartSpeed:2000,
+    responsive:{
+        0:{
+            items:1
+        },
+        900:{
+            items:2
+        },
+        1100:{
+            items:3
+        }
+    }
+})
+
+var owl1 = $('.carousel-proyectos');
+owl1.owlCarousel();
+// Go to the next item
+$('.NBtn').click(function() {
+    owl1.trigger('next.owl.carousel');
+    owl1.trigger('stop.owl.autoplay')
+    owl1.trigger('play.owl.autoplay',[6000])
+})
+// Go to the previous item
+$('.PBtn').click(function() {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl1.trigger('prev.owl.carousel');
+    owl1.trigger('stop.owl.autoplay')
+    owl1.trigger('play.owl.autoplay',[6000])
+})
+
+owl1.on('click', '.item-carousel', function () {
+    var nid=this.id;
+    owls.trigger('to.owl.carousel',nid);
+});
+
+$('.carousel-lightbox').owlCarousel({
+    loop:true,
+    margin:10,
+    dots:false,
+    touchDrag: false,
+    mouseDrag:false,
+    smartSpeed:2000,
+    items:1
+})
+
+var owls = $('.carousel-lightbox');
+owls.owlCarousel();
+// Go to the next item
+$('.SigBtn').click(function() {
+    owls.trigger('next.owl.carousel');
+})
+// Go to the previous item
+$('.AntBtn').click(function() {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owls.trigger('prev.owl.carousel');
+})
--- JS-MAPER/jsindex.js
+++ JS-MAPER/jsindex.js
@@ -1,3 +1,14 @@
+$('.menu2').hover(function(){
+    $('#as').css({color:'black'});
+},function(){
+    $('#as').css({color:'#999999'});})
+
+$('#as').hover(function(){
+    $(this).css({color:'black'});
+},function(){
+    $(this).css({color:'#999999'});
+})
+
 $('.carousels').owlCarousel({
     loop:true,
     margin:10,
@@ -120,69 +131,10 @@ $('.carousel4').owlCarousel({
         }
     }
 })
-$('.carousel-proyectos').owlCarousel({
-    loop:true,
-    margin:10,
-    dots:false,
-    autoplay:true,
-    autoplayTimeout: 6000,
-    touchDrag: false,
-    mouseDrag:false,
-    smartSpeed:2000,
-    responsive:{
-        0:{
-            items:1
-        },
-        900:{
-            items:2
-        },
-        1100:{
-            items:3
-        }
-    }
-})
 
-var owl1 = $('.carousel-proyectos');
-owl1.owlCarousel();
-// Go to the next item
-$('.NBtn').click(function() {
-    owl1.trigger('next.owl.carousel');
-    owl1.trigger('stop.owl.autoplay')
-    owl1.trigger('play.owl.autoplay',[6000])
-})
-// Go to the previous item
-$('.PBtn').click(function() {
-    // With optional speed parameter
-    // Parameters has to be in square bracket '[]'
-    owl1.trigger('prev.owl.carousel');
-    owl1.trigger('stop.owl.autoplay')
-    owl1.trigger('play.owl.autoplay',[6000])
-})
 
-owl1.on('click', '.item-carousel', function () {
-    var nid=this.id;
-    owls.trigger('to.owl.carousel',nid);
-});
-
-$('.carousel-lightbox').owlCarousel({
-    loop:true,
-    margin:10,
-    dots:false,
-    touchDrag: false,
-    mouseDrag:false,
-    smartSpeed:2000,
-    items:1
-})
-
-var owls = $('.carousel-lightbox');
-owls.owlCarousel();
-// Go to the next item
-$('.SigBtn').click(function() {
-    owls.trigger('next.owl.carousel');
-})
-// Go to the previous item
-$('.AntBtn').click(function() {
-    // With optional speed parameter
-    // Parameters has to be in square bracket '[]'
-    owls.trigger('prev.owl.carousel');
-})
+window.onload=function(){
+    $('#contenedor-precarga').animate({
+        bottom:'100%' 
+     })
+}
