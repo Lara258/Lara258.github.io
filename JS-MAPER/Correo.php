601b77c10db06a9f1885a08b53c68dab340bd9bc
Lara258 <lara.luisla@gmail.com>
2020-11-23 17:12:18 -0600

    prueba 1

--- JS-MAPER/Correo.php
+++ JS-MAPER/Correo.php
@@ -7,5 +7,6 @@
 
     $cabecera="Quiero una cotizacion de mi proyecto";
     $contenidomensaje="Nombre: ". $nombre . "\nCorreo: " .$correo . "\nTelefono: " . $telefono . "\nMensaje: " . $mensaje;
+    mail($destino,$cabecera,$contenidomensaje);
     echo "<script> alert('mensaje enviado con exito')";
 ?>
