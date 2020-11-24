744baad010b5c4c4cd0cfda7b65fd3b7a4d647be
Lara258 <lara.luisla@gmail.com>
2020-11-24 13:09:41 -0600

    prueba con phpmailer

--- Servicios/Correo.php
+++ Servicios/Correo.php
@@ -1,7 +1,48 @@
-
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '/phpmailer/Exception.php';
+require '/phpmailer/PHPMailer.php';
+require '/phpmailer/SMTP.php';
+
+$mail = new PHPMailer(true);
  
-if($_POST) {
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'Prueba.correo.ma@gmail.com';                     // SMTP username
+    $mail->Password   = 'mguHX96qqztGGXD';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('Prueba.correo.ma@gmail.com', 'Informacion MAPER');
+    $mail->addAddress('huichi_luis@hotmail.com', );     // Add a recipient
+
+    // Attachments
+   // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Prueba correo';
+    $mail->Body    = 'Este es un correo de prueba';
+
+    $mail->send();
+    echo 'El mensaje se ha enviado correctamente';
+} catch (Exception $e) {
+    echo "Hubo un error al enviar el mensaje, intentelo de nuevo mas tarde. Error del mensaje: {$mail->ErrorInfo}";
+}
+
+?>
+
+ //You will get a reply within 24 hours
+ //We are sorry but the email did not go through
+ if($_POST) {
     $nombre=$_POST['nombre'];
     $correo=$_POST['correo'];
     $destino='huichi_luis@hotmail.com';
@@ -23,20 +64,3 @@ if($_POST) {
     if(isset($_POST['mensaje'])) {
         $mensaje = htmlspecialchars($_POST['mensaje']);
     }
-     
-    $headers  = 'MIME-Version: 1.0' . "\r\n"
-    .'Content-type: text/html; charset=utf-8' . "\r\n"
-    .'From: ' . $correo . "\r\n";
-     
-    if(mail($destino,$cabecera,$contenidomensaje, $headers)) {
-        echo "<p>Gracias por mandarnos un mensaje: , $nombre. Nos pondremos en contacto con usted lo mas rapido posible.</p>";
-    } else {
-        echo '<p>Lo sentimos, pero su mensaje no pudo ser enviado, por favor intentelo de nuevo.</p>';
-    }
-     
-} else {
-    echo '<p>Algo ha salido mal, intente de nuevo mas tarde</p>';
-}
- //You will get a reply within 24 hours
- //We are sorry but the email did not go through
-?>
